1488f144de1b44eec9065fe90f1768f5c14e676d
gn01658704 <gn01658704@gmail.com>
2020-08-30 00:33:30 +0800

    .

--- js/gacha.js
+++ js/gacha.js
@@ -15,6 +15,7 @@ class Gacha {
         // var that = this
         this.awardList = [];
         this.canvas.getContext('2d').clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.canvas.getContext('2d').fillRect(0,0,this.canvas.width,this.canvas.height)
         for (let i = 0; i < this.ballRes.length; i++) {
             var ball = document.getElementById(this.ballRes[i]);
             this.awardList[i] = new Ball(this.canvas, i, ball);
@@ -33,14 +34,20 @@ class Gacha {
         window.clearInterval(this.timer);
         this.timer = setInterval(function () {
             that.canvas.getContext('2d').clearRect(0, 0, that.canvas.width, that.canvas.height);
+            that.canvas.getContext('2d').fillRect(0,0,that.canvas.width,that.canvas.height)
             for (let i = 0; i < that.awardList.length; i++) {
                 that.awardList[i].run();
             }
         }, 10);
 
-        setTimeout(() => {
-            that.stop(ball)
-        }, 1000)
+        // that.canvas.getContext('2d').clearRect(0, 0, that.canvas.width, that.canvas.height);
+        // for (let i = 0; i < that.awardList.length; i++) {
+        //     that.awardList[i].run();
+        // }
+
+        // setTimeout(() => {
+        //     that.stop(ball)
+        // }, 10000)
 
         $('.ballfalling').bind("webkitAnimationEnd oAnimationEnd msAnimationEnd animationend", function (event) {
             callback(ball)
@@ -68,9 +75,9 @@ function Ball(canvas, index, img) {
     this.canvas = canvas;
     this.ctx = canvas.getContext('2d');
     this.img = img;
-    this.r = this.img.width / 2;
-    this.x = this.rand(canvas.width - this.r * 2);
-    this.y = this.rand(canvas.height / 2 - this.r * 2);
+    this.r = this.img.width;
+    this.x = this.rand(canvas.width - this.r);
+    this.y = this.rand(canvas.height / 2 - this.r);
     this.y = this.y + canvas.height / 2
     this.type = index;
 
@@ -94,17 +101,23 @@ Ball.prototype = {
     run: function () {
         this.x += this.speedX;
         this.y += this.speedY;
-        if (this.x > this.canvas.width - this.r * 2) {
+
+
+        if (this.x > this.canvas.width - this.r) {
             this.speedX = -this.speedX;
+            this.x = this.canvas.width - this.r;
         }
         if (this.x < 0) {
             this.speedX = Math.abs(this.speedX);
+            this.x = 0;
         }
-        if (this.y > this.canvas.height - this.r * 2) {
+        if (this.y > this.canvas.height - this.r) {
             this.speedY = -this.speedY;
+            this.y = this.canvas.height - this.r;
         }
         if (this.y < 0) {
             this.speedY = Math.abs(this.speedY);
+            this.y = 0;
         }
         this.ctx.drawImage(this.img, this.x, this.y, this.img.width, this.img.height);
     }
